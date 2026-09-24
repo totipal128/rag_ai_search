1252a0b6703e62d71c3b56cc20ee41989be49982
Ismail Totipal <ismailtotipal1@gmail.com>
2026-09-24 23:26:05 +0700

    Tambah pagination untuk ReportViewSet

--- apps/report/views.py
+++ apps/report/views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from apps.report.models import Report
+from apps.report.pagination import ReportPagination
 from apps.report.serializers import ReportSerializer
 
 
@@ -10,3 +11,4 @@ class ReportViewSet(ModelViewSet):
     serializer_class = ReportSerializer
     permission_classes = [AllowAny]
     authentication_classes = []
+    pagination_class = ReportPagination
